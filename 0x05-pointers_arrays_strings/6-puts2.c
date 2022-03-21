f0d82e9abff4d0958cea14030bcb26570976afdd
Yacob Tsegaye <yacobhareg@gmail.com>
2022-03-21 15:59:26 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 {
 	int j;
 
-	for (j = 0; str[j] != '\0';)
+	for (j = 0; str[j] != '\0'; j++)
 	{
 		if ((j%2) == 0)
 			_putchar(str[j]);
+		else
+			continue;
 	}
 	_putchar('\n');
 }
