3de7183bbbb6027d7991ecf1d635a0fff65188d3
Yacob Tsegaye <yacobhareg@gmail.com>
2022-03-21 15:00:05 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: prints the string in reverse
+*/
+void print_rev(char *s)
+{
+	int j;
+	int len;
+
+	len = _strlen(s);
+	for (j = len; j = 0; j--)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
