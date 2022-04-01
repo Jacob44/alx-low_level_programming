44d4785224e1c6cacedf890bd7599fc8e02319c7
Yacob <yacobhareg@gmail.com>
2022-04-02 00:53:12 +0300

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers and prints the result.
+ * @argc: number of commands
+ * @argv: the commands
+ *
+ * Return: 0 if there is no error. 1 if the arguments are not number.
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0;
+	int i = 1;
+	int intc;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		intc = atoi(argv[i]);
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			add += intc;
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
+
