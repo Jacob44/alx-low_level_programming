97a2fb8ea92fe0afdebb148f6b78d57574229071
Yacob <yacobhareg@gmail.com>
2022-04-12 10:25:00 +0300

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compliled form.
+ *
+ * Return: nothing.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
