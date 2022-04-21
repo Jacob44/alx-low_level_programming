fbb675a77a73471402ff47ee391f5ceb34a3e881
Yacob <yacobhareg@gmail.com>
2022-04-21 23:44:11 +0300

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,17 +4,19 @@
 /**
  * free_list - wfrees a list_t list
  * @head: pointer to a list_t pointer that points to the head struct
- * 
  * Return: Nothing
  */
 void free_list(list_t *head)
 {
 	list_t *tmp;
 
+	tmp = head;
 	while (head != NULL)
 	{
 		tmp = head;
 		head = head->next;
+		free(tmp->str);
 		free(tmp);
 	}
+	free(head);
 }
