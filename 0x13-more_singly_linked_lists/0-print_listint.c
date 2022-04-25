f471495b7243a4e5ff9a7f80e89a98e1305ac865
Yacob <yacobhareg@gmail.com>
2022-04-26 00:39:59 +0300

    0 print list integer

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a listint_t list.
@@ -9,8 +10,6 @@
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
-	if (h == NULL)
-		return (0);
 
 	while (h)
 	{
