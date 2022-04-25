6cf12952125872403f9761dc229c10e892ca9ad3
Yacob <yacobhareg@gmail.com>
2022-04-25 23:28:16 +0300

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: prints the elements
+ *
+ * Return: the number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
